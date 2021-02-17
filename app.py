b4c020b9d91c0236b632ebe3e2da954791e7c792
Stephane Caron <ste.caron@icloud.com>
2021-02-17 18:12:20 -0500

    Minor update to my texte

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 from flask import Flask
 import os
 import socket
@@ -6,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def hello():
-    html = "<h3>Bonne fete JT!!</h3> Voici une petite et tres modeste carte d'anniversaire de la part de quelques gailles de .Layer <br/> <b>Steph Caron</b> <br/> Joyeux 35e anniversaire mon JT. Si t'etais nee 2 jours plus tot, t'aurais la meme date de fete que le covid quebecois. Sur une note plus serieuse et moins covideuse, si j'avais a choisir la plus belle chose que .Layer m'a apporte, c'est d'avoir eu la chance de rencontrer et de devenir amis avec des gens comme toi! Je m'ennuie de passer du temps avec vous tous. J'espere que tout va bien de ton cote. On se revoit bientot j'espere ... <br/> <b>Sam Perochkin</b> <br/> JeeTee, l'inventeur du nom .Layer quoi. On s'est pas trop vu dans la dernière année et ça c'est mauvais signe. J'espère que toi, tes proches et tes main/side projets vont bien et qu'on pourra organiser de quoi real life cet été. Je vais enfiler mon chandail catnip pour l'occasion. (En fait, je le mets presque toujours haha). Je t'en souhaite une bonne fellow .Layeurien!"
+    html = "<h3>Bonne fête JT!!</h3> Voici une petite et très modeste carte d'anniversaire de la part de quelques gailles de .Layer <br/> <br/> <b>Steph Caron</b> <br/> Joyeux 35e anniversaire mon JT. Si t'étais né 2 jours plus tôt, t'aurais la même date de fête que le covid québécois. Sur une note plus sérieuse et moins covideuse, je dois t'avouer que si j'avais a choisir la plus belle chose que .Layer m'a apporté, c'est d'avoir eu la chance de rencontrer et de devenir amis avec des gens comme toi! Je m'ennuie de passer du temps avec vous tous. J'espère que tout va bien de ton côté dans tous tes projets de vie. On se revoit bientot j'espere ... <br/> <br/> <b>Sam Perochkin</b> <br/> JeeTee, l'inventeur du nom .Layer quoi. On s'est pas trop vu dans la dernière année et ça c'est mauvais signe. J'espère que toi, tes proches et tes main/side projets vont bien et qu'on pourra organiser de quoi real life cet été. Je vais enfiler mon chandail catnip pour l'occasion. (En fait, je le mets presque toujours haha). Je t'en souhaite une bonne fellow .Layeurien!"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
 if __name__ == "__main__":
